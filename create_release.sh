cc51d737ec6798a94e32681e223d5024d1ae652e
Beinsezii <beinsezii@gmail.com>
2020-05-25 15:15:06 -0700

    create_release.sh now uses * completion 'bsz-*/*.py' since this is the millionth time I've forgotten to add a new dir

--- create_release.sh
+++ create_release.sh
@@ -5,8 +5,6 @@ printf -v date '%(%Y-%m-%d)T'
 zip -q bsz-gimp-plugins_${date}.zip \
     bsz_gimp_lib.py \
     bszgw.py \
-    bsz-dualbloom/bsz-dualbloom.py \
-    bsz-dualbloom2/bsz-dualbloom2.py \
-    bsz-filmic-chroma/bsz-filmic-chroma.py \
+    bsz-*/*.py
 
 git tag ${date}
