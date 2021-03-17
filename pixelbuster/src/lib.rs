1f3a2c4e6d904f129cad51e27f9024bf4bf3f313
Beinsezii <beinsezii@gmail.com>
2021-03-16 18:15:36 -0700

    Added rust version of benches using nightly bench
    Simply run `cargo bench`. Same benches as python. Helpful for quickly
    finding small performance gains in the rust side of code. Also runs
    multiple tests by default.

--- pixelbuster/src/lib.rs
+++ pixelbuster/src/lib.rs
@@ -1,3 +1,5 @@
+#![feature(test)]
+extern crate test;
 use std::os::raw;
 use std::sync::Arc;
 
@@ -281,3 +283,79 @@ pub extern "C" fn pixelbuster(
         t.join().expect("Thread fail");
     }
 } // }}}
+
+#[cfg(test)]
+mod tests {
+    use crate::pixelbuster;
+    use std::ffi::CString;
+    use std::os::raw;
+
+    #[bench]
+    fn bench_simple(b: &mut test::bench::Bencher) {
+        let count = 4000 * 3000 * 4;
+        let mut pixels = Vec::<f64>::new();
+        pixels.reserve(count);
+        for _ in 0..count {pixels.push(rand::random::<f64>())}
+        b.iter(|| {
+            pixelbuster(
+                CString::new("r + 1\n\
+                r + 1\n\
+                r + 1\n\
+                r + 1\n\
+                r + 1\n\
+                r - 5").unwrap().into_raw(),
+                CString::new("rgba").unwrap().into_raw(),
+                pixels.as_mut_ptr().cast::<raw::c_char>(),
+                count,
+            );
+        });
+    }
+
+    #[bench]
+    fn bench_complex(b: &mut test::bench::Bencher) {
+        let count = 4000 * 3000 * 4;
+        let mut pixels = Vec::<f64>::new();
+        pixels.reserve(count);
+        for _ in 0..count {pixels.push(rand::random::<f64>())}
+        b.iter(|| {
+            pixelbuster(
+                CString::new("v = r\n\
+                r + pi\n\
+                r - pi\n\
+                r * 2\n\
+                r / 2\n\
+                r sqrt r\n\
+                r pow 2\n\
+                r min 100\n\
+                r max 0\n\
+                r abs r\n\
+                r log e\n\
+                r round r\n\
+                r = v").unwrap().into_raw(),
+                CString::new("rgba").unwrap().into_raw(),
+                pixels.as_mut_ptr().cast::<raw::c_char>(),
+                count,
+            );
+        });
+    }
+
+    #[bench]
+    fn bench_real(b: &mut test::bench::Bencher) {
+        let count = 4000 * 3000 * 4;
+        let mut pixels = Vec::<f64>::new();
+        pixels.reserve(count);
+        for _ in 0..count {pixels.push(rand::random::<f64>())}
+        b.iter(|| {
+            pixelbuster(
+                CString::new("v = 100\n\
+                v2 = l\n\
+                v2 / 1.25\n\
+                v - v2\n\
+                c * v").unwrap().into_raw(),
+                CString::new("rgba").unwrap().into_raw(),
+                pixels.as_mut_ptr().cast::<raw::c_char>(),
+                count,
+            );
+        });
+    }
+}
